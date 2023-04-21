2362583405e92ee1f2052a5f475916e7799624d9
Solveiga <solveiga.galatiltyte@bjss.com>
2023-04-21 15:39:27 +0100

    delete individual task

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const addForm = document.querySelector(".add");
 const tasks = document.querySelector(".tasks");
 
+
 addForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const value = addForm.task.value.trim();
@@ -10,6 +11,17 @@ addForm.addEventListener("submit", (event) => {
         <span>${value}</span>
         <i class="bi bi-trash-fill delete"></i>
     </li>`;
+    //adding a list item into the tasks div
     addForm.reset();
+    //reseting the input
   }
 });
+
+
+tasks.addEventListener("click", event => {
+    //event listener on entire div due to event listener not working on the freshly added tasks without reloading the page? 
+    if (event.target.classList.contains("delete")) {
+        //logic that ensures task is only removed when correct part of element is clicked
+        event.target.parentElement.remove();
+    }
+})
